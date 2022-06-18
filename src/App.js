a8507dce8962e460ab198e3f659492ee4898dc35
Senatauro <cgsste@hotmail.com>
2022-06-18 10:58:55 -0300

    initial commit
    A order summary component made in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import imgHeader from './images/illustration-hero.svg';
+import imgMusic from './images/icon-music.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='order-summary'>
+        <img src={imgHeader} alt="Illustration of a woman listening to music while dancing with a city in the background" />
+        <div className='order-summary-info'>
+          <h1 className='order-summary-title'>Order summary</h1>
+          
+          <p className='order-summary-desc'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+          
+          <div className='order-summary-plan-button'>
+            <img src={imgMusic} alt="Icon of a music note" />
+            <div className='order-summary-plan-button-info'>
+              <p className='order-summary-plan-button-plan'>Annual Plan</p>
+              <p className='order-summary-plan-button-price'>$59,99/year</p>
+            </div>
+            <p className='order-summary-plan-button-change'>Change</p>
+          </div>
+
+          <button className="order-summary-payment-button">
+            Proceed to Payment
+          </button>
+          <p className='order-summary-cancel'>
+            Cancel Order
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
